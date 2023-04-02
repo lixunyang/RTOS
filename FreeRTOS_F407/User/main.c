2f1dbe9f7e61e4a45d5082f92f028728cb7738e3
lixunyang <1043501543@qq.com>
2023-04-02 18:40:06 +0800

    上传FreeRTOS项目工程代码文件

---
+++ FreeRTOS_F407/User/main.c
@@ -0,0 +1,61 @@
+/**
+ ****************************************************************************************************
+ * @file        main.c
+ * @author      正点原子团队(ALIENTEK)
+ * @version     V1.0
+ * @date        2021-11-04
+ * @brief       内存管理 实验
+ * @license     Copyright (c) 2020-2032, 广州市星翼电子科技有限公司
+ ****************************************************************************************************
+ * @attention
+ *
+ * 实验平台:正点原子 探索者 F407开发板
+ * 在线视频:www.yuanzige.com
+ * 技术论坛:www.openedv.com
+ * 公司网址:www.alientek.com
+ * 购买地址:openedv.taobao.com
+ *
+ ****************************************************************************************************
+ */
+
+#include "./SYSTEM/sys/sys.h"
+#include "./SYSTEM/usart/usart.h"
+#include "./SYSTEM/delay/delay.h"
+#include "./BSP/LED/led.h"
+#include "./BSP/LCD/lcd.h"
+#include "./BSP/KEY/key.h"
+#include "./BSP/SRAM/sram.h"
+#include "./MALLOC/malloc.h"
+#include "freertos_demo.h"
+
+const char *SRAM_NAME_BUF[SRAMBANK] = {" SRAMIN  ", " SRAMCCM ", " SRAMEX  "};
+
+int main(void)
+{
+    HAL_Init();                         /* 初始化HAL库 */
+    sys_stm32_clock_init(336, 8, 2, 7); /* 设置时钟,168Mhz */
+    delay_init(168);                    /* 延时初始化 */
+    usart_init(115200);                 /* 串口初始化为115200 */
+    led_init();                         /* 初始化LED */
+    lcd_init();                         /* 初始化LCD */
+    key_init();                         /* 初始化按键 */
+    sram_init();                        /* SRAM初始化 */
+    
+    my_mem_init(SRAMIN);                /* 初始化内部SRAM内存池 */
+    my_mem_init(SRAMEX);                /* 初始化外部SRAM内存池 */
+    my_mem_init(SRAMCCM);               /* 初始化内部CCM内存池 */
+#if 0
+    lcd_show_string(30,  50, 200, 16, 16, "STM32", RED);
+    lcd_show_string(30,  70, 200, 16, 16, "MALLOC TEST", RED);
+    lcd_show_string(30,  90, 200, 16, 16, "ATOM@ALIENTEK", RED);
+    lcd_show_string(30, 110, 200, 16, 16, "KEY0:Malloc & WR & Show", RED);
+    lcd_show_string(30, 130, 200, 16, 16, "KEY_UP:SRAMx KEY1:Free", RED);
+    lcd_show_string(60, 160, 200, 16, 16, " SRAMIN ", BLUE);
+    lcd_show_string(30, 176, 200, 16, 16, "SRAMIN   USED:", BLUE);
+    lcd_show_string(30, 192, 200, 16, 16, "SRAMCCM  USED:", BLUE);
+    lcd_show_string(30, 208, 200, 16, 16, "SRAMEX   USED:", BLUE);
+#endif  
+		freertos_demo();
+}
+
+
---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+https://github.com/lixunyang/RTOS.git
+
+
+
+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
