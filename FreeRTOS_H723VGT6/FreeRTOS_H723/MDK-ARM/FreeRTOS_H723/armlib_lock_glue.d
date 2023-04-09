d1aa480d1688c6587fd7f695b1c135eada9df8fd
lixunyang <1043501543@qq.com>
2023-04-09 13:28:15 +0800

    1、更新基础工程

--- FreeRTOS_H723VGT6/FreeRTOS_H723/MDK-ARM/FreeRTOS_H723/armlib_lock_glue.d
+++
@@ -1,6 +0,0 @@
-freertos_h723\armlib_lock_glue.o: armlib_lock_glue.c
-freertos_h723\armlib_lock_glue.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
-freertos_h723\armlib_lock_glue.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-freertos_h723\armlib_lock_glue.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
-freertos_h723\armlib_lock_glue.o: stm32_lock.h
-freertos_h723\armlib_lock_glue.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
--- FreeRTOS_H723VGT6/FreeRTOS_H723/MDK-ARM/FreeRTOS_H723/startup_stm32h723xx.d
+++
@@ -1,1 +0,0 @@
-freertos_h723\startup_stm32h723xx.o: startup_stm32h723xx.s
